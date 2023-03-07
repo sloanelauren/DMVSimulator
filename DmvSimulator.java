adc4d2e771d0b7d66a4d9ba1d12460692bf655d6
sloanelauren <sloanelaurenm@gmail.com>
2023-03-07 15:59:25 -0500

    Mircale num

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -3,7 +3,6 @@ public class DmvSimulator {
     public static void main(String[] args) {
 
         Random rand = new Random();
-
         int ticket = rand.nextInt(200);
 
         System.out.println("\nHello! Welcome to the DMV!");
@@ -13,15 +12,20 @@ public class DmvSimulator {
         System.out.println(" -----------------");
 
         int count = ticket + 1;
-        while (count != ticket) {
+    while (count != ticket) {
              System.out.println(count);
              count ++;
          if (count == 201 ) {
-            count = 100;
+            count = 1;
         } else if (ticket == count) {
             System.out.println(ticket);
+                int magicnum = rand.nextInt(100) + 1;
+            if(magicnum == 15) {
+                System.out.println("Oh my favorite number! You have the correct paperwork. Thank you!");
+            } else {
             System.out.println("We can't accept this paperwork, get to the back of the line or come back tomorrow.");
-        }
-        }
+            }
+        }   
     }
+}
 }
